4c0866e928232d40f096796504a629bf9ca73ce0
ludovicbreger <lb123457@gmail.com>
2017-11-25 21:13:22 -0800

    Minor changes

--- src/python/create_test_data.py
+++ src/python/create_test_data.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import numpy as np
 
-df = pd.DataFrame(np.random.randint(0,100,size=(30000000, 4)), columns=list('ABCD'))
+df = pd.DataFrame(np.random.randint(0,100,size=(30000, 4)), columns=list('ABCD'))
 
 print(df)
 
 df.to_csv('~/Development/Data/dummy.csv', index=False, encoding='utf-8')
 
-print(1)
+print('Done writing file')
 
